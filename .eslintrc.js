b4fd89b75ccc29bf50d724013daa9408eced1646
gness1804 <graham.nessler@gmail.com>
2021-05-23 16:53:35 -0500

    Init setup

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+  root: true,
+  /* only if using TypeScript. If so, run 'yarn add @typescript-eslint/parser' */
+  // parser: '@typescript-eslint/parser',
+  plugins: [
+    /* only if using TypeScript. If so, run 'yarn add @typescript-eslint/eslint-plugin' */
+    // '@typescript-eslint',
+    /* only if using React. If so, run 'yarn add eslint-plugin-react' */
+    // 'react',
+  ],
+  parserOptions: {
+    // will be used most of the time as I typically work in module-based frameworks
+    sourceType: 'module',
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      //  only if using JSX
+      // jsx: true
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    // only if using TypeScript
+    // 'plugin:@typescript-eslint/recommended',
+    // only if using React
+    // 'plugin:react/recommended',
+  ],
+  rules: {
+    'no-console': 'warn',
+    // the two below are only if using TypeScript
+    // '@typescript-eslint/ban-types': 'warn',
+    // '@typescript-eslint/no-unused-vars': 'error',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+}
+
